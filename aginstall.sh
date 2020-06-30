91374b2ecbf43314ce3c6c80aec87d2580e4bd75
Jay Desai <52437445+jay-nginx@users.noreply.github.com>
2020-06-30 15:51:06 +1000

    Deleted agentinstall.sh
    Deleted agent install file

--- aginstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ansible-playbook -i inventory-hosts.yml ctr-agent-install-playbook.yml -e "api_key=fakegidfb614c8b56e7098a8e4aff398f656acri nginx_controller_fqdn=cts.giri.local nginx_controller_location=india-zone-east nginx_controller_instance_name=debserver"
-
